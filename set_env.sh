b77066245eae3fb108cfd5561e4c5133086a32e3
Handagaurav <Handagaurav@users.noreply.github.com>
2020-07-19 08:44:18 -0400

    fix

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=udagramgaurav
-export POSTGRES_PASSWORD=grh25101993
-export POSTGRES_HOST=udagramgaurav.c0ibi3gf0pbo.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=udagramgaurav
-export AWS_BUCKET=udagramgaurav
+export POSTGRES_USERNAME=microservicesp3
+export POSTGRES_PASSWORD=microservicesp3
+export POSTGRES_HOST=microservicesp3.c0ibi3gf0pbo.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=microservicesp3
+export AWS_BUCKET=microservicesp3
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=abcd
