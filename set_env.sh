25e2c10e3ad6731f5b10eaa3f5964a553fea8b09
Handagaurav <Handagaurav@users.noreply.github.com>
2020-07-19 06:20:38 -0400

    chore

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramgaurav
+export POSTGRES_PASSWORD=grh25101993
+export POSTGRES_HOST=udagramgaurav.c0ibi3gf0pbo.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagramgaurav
+export AWS_BUCKET=udagramgaurav
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=abcd
+export URL=http://localhost:8100
